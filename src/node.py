d4b691d95e5a7c4e8d46a48f51082858e2b4c86b
Kashish Arora <kashisharora99@gmail.com>
2021-10-17 18:48:08 -0400

    Create node class

---
+++ src/node.py
@@ -0,0 +1,23 @@
+class Node {
+    """
+    Stores information about one point on the map including:
+        - latitude
+        - longitude
+        - neighbors (the points we can go from this point)
+        - elevation
+    """
+    def __init__(self, latitude, longitude, neigbors=[], elevation=0):
+        self.latitude = latitude
+        self.longitude = longitude
+        self.neigbors = neigbors
+        self.elevation = elevation
+    
+    def get_coordinates(self):
+        return (self.latitude, self.longitude)
+    
+    def get_neighbors(self):
+        return self.neigbors
+    
+    def get_elevation(self):
+        return self.elevation
+}
