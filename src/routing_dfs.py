bc65a694386124bc80c19bd023bc7187da870312
jye6013 <jye6013@bths.edu>
2021-11-18 16:50:32 -0500

    update dfs routing

--- src/routing_dfs.py
+++ src/routing_dfs.py
@@ -1,6 +1,6 @@
 import osmnx
 
-def dfs(graph, start, end, x, elevation_setting=None):
+def dfs(graph, start, end):
     """
         Runs DFS path algorithm from start to end location to find shortest path
 
@@ -8,8 +8,6 @@ def dfs(graph, start, end, x, elevation_setting=None):
             graph: networkx multidigraph - the area we are searching in
 		    start: int - the starting location of the route
 		    end: int - the end location of the route
-		    x: int - the percentage we can deviate from the shortest path length
-		    elevation_setting: string - either "maximize", "minimize", or None
 
         return: list - a route from start to end or None if a route does not exist
     """
@@ -37,20 +35,6 @@ def dfs(graph, start, end, x, elevation_setting=None):
                     return get_path_from_previous_nodes(previous_nodes, start, end)
     
     return None
-
-def get_total_path_length(nodes, graph):
-    """
-	Calculates the total length of the path containing `nodes`. 
-	params:
-		nodes: list of ints (node IDs)
-		graph: networkx multidigraph - the area we are searching in, contains `nodes`
-	return: int, the total distance from start to end node in the path
-	"""
-    distance = 0
-
-    for i in range(len(nodes)-1):
-	    distance += graph.edges[nodes[i], nodes[i+1], 0]['length']
-    return distance
 
 def get_path_from_previous_nodes(previous_nodes, start, end):
 	"""
