6da6b1ea28f87bdfa76a8c0e7c6fa49b428d89dc
Kashish Arora <kashisharora99@gmail.com>
2021-12-02 18:38:22 -0500

    Display path

--- src/app.py
+++ src/app.py
@@ -1,6 +1,9 @@
 import osmnx
 import routing
 import pickle as pkl
+import networkx as nx
+import matplotlib.pyplot as plt
+
 
 class App:
 	def __init__(self):
@@ -12,6 +15,7 @@ class App:
 		self.graph = None
 		self.TRANSPORTATION_MODES = ["drive", "walk", "bike"]
 		self.ELEVATION_MODES = ["maximize", "minimize", ""]
+		self.ROUTING_METHODS = ["dijkstra", "a*"]
 		self.start = None
 		self.end = None
 		self.routing_method = None
@@ -19,7 +23,11 @@ class App:
 	def set_user_inputs(self):
 		self.start_address = input("Enter the address of your start location: ")
 		self.end_address = input("Enter the address of your end location: ")
-		self.routing_method = input("Enter the routing algorithm you would like to use (BFS, DFS, Dijkstra, A*): ").lower()
+		self.routing_method = input("Enter the routing algorithm you would like to use (Dijkstra, A*): ").lower()
+
+		while self.routing_method not in self.ROUTING_METHODS:
+			self.routing_method = input("Enter the routing algorithm you would like to use (Dijkstra, A*): ").lower()
+
 		self.elevation_gain_mode = input("Type 'maximize' if you want to maximize elevation gain, or 'minimize' if you want to minimize elevation gain (no quotes), or press enter to skip & to get the shortest route: ")
 		while self.elevation_gain_mode not in self.ELEVATION_MODES:
 			self.elevation_gain_mode = input("Please enter a valid option between maximize, minimize, or enter to skip and get shortest route: ")
@@ -63,13 +71,31 @@ class App:
 		else:
 			print("Invalid routing method selected.")
 			return None
-    
+
+	def display_path(self, path):
+		positions = {}
+
+		for node in list(self.graph.nodes(data=True)):
+			xy = []
+
+			xy.append(node[1]['x'])
+			xy.append(node[1]['y'])
+
+			positions[int(node[0])] = xy
+
+		h = self.graph.subgraph(path)
+		nx.draw_networkx_nodes(h,pos=positions, node_color='b', node_size=5) #or even nx.draw(h,pos=pos,node_color='b') to get nodes and edges in one command
+		nx.draw_networkx_edges(h,pos=positions)
+		plt.axis('equal')
+		plt.show() 
+
 def main():
 	app = App()
 	app.set_user_inputs()
 	app.set_graph()
 	app.set_start_end_nodes()
-	app.find_route()
+	path = app.find_route()
+	app.display_path(path)
 
 if __name__ == '__main__':
 	main()
